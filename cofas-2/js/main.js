8ba0ed81cca1a7a92886dc8bbc0d7790e24ad1f6
Sai Vineet <saivineet89@gmail.com>
2014-10-25 19:53:23 +0530

    Changes

--- cofas-2/js/main.js
+++ cofas-2/js/main.js
@@ -1,12 +1,15 @@
-$('a').click(function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1000);
-    return false;
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
-$('.scroll').click(function(){
-    $("html, body").animate({ scrollTop: $(window).height()}, 1000);
-    return false;
-});
